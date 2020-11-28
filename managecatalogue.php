d0e5c7bfe03dcbe1838d210970e27f463dfb924b
retxedchua <72144948+retxedchua@users.noreply.github.com>
2020-11-28 12:39:52 +0800

    Catalogue

--- managecatalogue.php
+++ managecatalogue.php
@@ -1,19 +1,27 @@
-<?php
-include 'sessiontest.php';
-include 'adminTraverseSecurity.php';
-?>
 <!DOCTYPE html>
 <html lang = "en">
     <head>
     <?php
     include 'header.php';
     ?>
-                <title>Floured - Manage Catalogue</title>
+        <title>Floured - Manage Items</title>
     </head>
     <body>
         <?php
-        include 'navbar.php';
+        include 'm.navbar.php';
         ?>
+
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Manage Items</h1>
+                <p class="lead">Navigate using the buttons below.</p>
+
+                <button type="button" onclick="window.location.href = 'm.additem.php'" class="btn btn-secondary btn-lg btn-block">Add New Item</button>
+
+                <button type="button" onclick="window.location.href = 'm.itemupdate.php'" class="btn btn-secondary btn-lg btn-block">Update/Remove Item Information</button>
+
+            </div>
+        </div>
 
     </body>
     <?php
