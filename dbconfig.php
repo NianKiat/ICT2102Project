9dd8d4912ee7e8d6b214e989bd329fc804a455cd
Hakim Razalee <hakimrazalee@gmail.com>
2020-12-01 21:50:19 +0800

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,14 +1,14 @@
 <?php
 function OpenCon()
  {
-// $dbhost = "localhost";
-// $dbuser = "sqldev";
-// $dbpass = "abc123";
-// $db = "Floured";
  $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "123";
- $db = "floured";
+ $dbuser = "sqldev";
+ $dbpass = "abc123";
+ $db = "Floured";
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "123";
+// $db = "floured";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
