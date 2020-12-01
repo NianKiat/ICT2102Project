61aece4586766b2665a7e621723c6402594fe5ce
Abcnian <phssniankiat@hotmail.com>
2020-12-01 12:17:30 +0800

    added admin manage delivery

---
+++ managedelivery.php
@@ -0,0 +1,125 @@
+<?php
+require 'dbconfig.php';
+include 'sessiontest.php';
+include 'adminTraverseSecurity.php';
+?>
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <?php
+        $conn = OpenCon();
+
+        // Check connection
+        if ($conn === false) {
+            die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
+        include 'header.php';
+        ?>
+        <title>Floured</title>
+    </head>
+    <body>
+        <?php
+        include 'navbar.php';
+        ?>
+        <br/>
+        <br/>
+        <br/>
+        <main class="container">        
+            <h1>View Delivery Date</h1>
+            <div class="card border-dark mb-3">
+                <div class="card-header"> &nbsp;Your Date</div>
+                <div class="card-body text-primary">
+                    <main role="main" class="col-md-9 ml-sm-auto col-lg-auto px-md-auto">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-sm" id="tabledate">
+                                <thead>
+                                    <tr>
+                                        <th>Member's name</th>
+                                        <th>Email</th>
+                                        <th>Cake</th>
+                                        <th>Quantity</th>
+                                        <th>Date</th>
+                                        <th>&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $status = 1;
+                                    //$id = $_SESSION['memberID'];
+                                    //$query2 = "SELECT * FROM checkout WHERE memberid = '$id'";
+                                    $query = "SELECT checkout.checkoutid, fmembers.fname, fmembers.email, checkout.name, checkout.quantity, checkout.date 
+FROM Floured.checkout
+INNER JOIN Floured.fmembers ON checkout.memberid = fmembers.memberid";
+                                    if ($result = $conn->query($query)) {
+                                   
+                                    while ($row = $result->fetch_assoc()) {
+                                        $field1id = $row["checkoutid"];
+                                        $fieldfname = $row["fname"];
+                                        $fieldemail = $row["email"];
+                                        $fieldcakename = $row["name"];
+                                        $fieldquantity = $row["quantity"];
+                                        $fielddate = $row["date"];
+                                        $status = 0;
+                                        ?>     
+                                        <tr id="delete<?php echo $field1id; ?>"> 
+                                            <td><?php echo $fieldfname; ?></td>
+                                            <td><?php echo $fieldemail; ?></td> 
+                                            <td><?php echo $fieldcakename; ?></td>
+                                            <td><?php echo $fieldquantity; ?></td>
+                                            <td><?php echo $fielddate; ?></td>
+<!--                                            <td style="text-align:right;">                                    
+                                                <form action="deliveryedit.php" method="POST">
+                                                    <button name="editdate" value="<?php echo $field1name; ?>" >Change date</button>
+                                                </form>
+                                            </td>-->
+                                            <td style="text-align:right;">                                         
+                                                <button name="deletedate" onclick="deleteAjax(<?php echo $field1id;  ?>)">Delivered Cake</button>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                    //$result->free();
+                                } else {
+                                    echo "<a>No Delivery Found</a>";
+                                }
+                                ?>
+                                </tbody>
+                                <script type="text/javascript">
+                                    function deleteAjax(id) {
+                                        $.ajax({
+                                            type: 'post',
+                                            url: 'deletedelivery.php',
+                                            data: {delete_id: id},
+                                            success: function (data) {
+                                                $('#delete' + id).hide('slow');
+                                            }
+                                        });
+                                    }
+                                </script>
+                                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+                            </table>
+                            <?php
+                                if($status == 1)
+                                {
+                                    ?>
+                            <a id="todelete">No Delivery cakes found</a>
+                            <?php
+                                }
+                                else
+                                {
+                                    echo "";
+                                }
+                            ?>
+                           
+                        </div>
+                </div>
+            </div>
+        </main>    
+    </body>
+<?php
+include 'footer.php';
+?>
+</html>
+
+
+
